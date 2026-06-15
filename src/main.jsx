e91cc43ea3c3b1671528f542f875e2d9b711d99c
mvadgroups <mvadgroups@gmail.com>
2026-06-15 21:01:23 +0530

    fix Links1 import

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-base: "/LOCFLUENCE-/"
+  base: "/LOCFLUENCE-/"
 })
